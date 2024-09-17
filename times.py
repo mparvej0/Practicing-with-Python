514ff45d1f0b27560f4095044e2c63c1419bba23
MD PARVEJ <mparvejind@gmail.com>
2024-09-18 01:51:10 +0530

    changing in timing code

--- times.py
+++ times.py
@@ -1,26 +1,14 @@
-import time
-timestamp = time.strftime("%H:%M:%S")
-print(timestamp)
-timestamp = time.strftime("%H")
-print(timestamp)
-timestamp = time.strftime("%M")
-print(timestamp)
-timestamp = time.strftime("%S")
-print(timestamp)
-
-import time
-t = time.strftime("%H:%M:%S")
-hour = int(time.strftime("%H"))
-print(hour)
-
-if(hour>=0 and hour<12):
-    print("Good Morning Raju!")
-elif(hour>=12 and hour<17):
-    print("Good Afternoon Raju!")
-elif(hour>=17 and hour<0):
-    print("Good Night Raju!")
-
 # Time and Date
 import time
 t = time.strftime("%Z\n%H:%M:%S %p\n%j Days\n%D %h\n%A %B")
 print(t)
+
+hour = int(time.strftime("%H"))
+if(hour>0 and hour<=12):
+    print("Good Morning Raju !")
+elif(hour>12 and hour<=4):
+    print("Good Afternoon Raju !")
+elif(hour>4 and hour<=19):
+    print("Good Evening Raju !")
+elif(hour>19 and hour<=4):
+    print("Good Night Raju !")
