ef272d2cdbb3b67b2b3f36e9726890650545263f
MD PARVEJ <mparvejind@gmail.com>
2024-09-19 21:28:58 +0530

    time else if updated

--- times.py
+++ times.py
@@ -4,11 +4,12 @@ t = time.strftime("%Z\n%H:%M:%S %p\n%j Days\n%D %h\n%A %B")
 print(t)
 
 hour = int(time.strftime("%H"))
-if(hour>0 and hour<=12):
+if(hour>4 and hour<=12):
     print("Good Morning Raju !")
-elif(hour>12 and hour<=4):
+elif(hour>12 and hour<=16):
     print("Good Afternoon Raju !")
-elif(hour>4 and hour<=19):
-    print("Good Evening Raju !")
-elif(hour>19 and hour<=4):
+if(hour>16 and hour<=19):
+    print("Good Evening !")
+else:
+    (hour>19 and hour<=4)
     print("Good Night Raju !")
