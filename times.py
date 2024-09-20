4f2e5a537038a085121af842bf932c7d98a0a776
MD PARVEJ <mparvejind@gmail.com>
2024-09-20 16:54:15 +0530

    new update in time&date

--- times.py
+++ times.py
@@ -1,15 +1,18 @@
-# Time and Date
 import time
-t = time.strftime("%Z\n%H:%M:%S %p\n%j Days\n%D %h\n%A %B")
+
+# Display the current time and date information
+t = time.strftime("%Z\n%I:%M:%S %p\n%j Days\n%d-%m-%Y %b\n%A %B")
 print(t)
 
+# Get the current hour as an integer in 24-hour format
 hour = int(time.strftime("%H"))
-if(hour>4 and hour<=12):
-    print("Good Morning Raju !")
-elif(hour>12 and hour<=16):
-    print("Good Afternoon Raju !")
-if(hour>16 and hour<=19):
-    print("Good Evening !")
-else:
-    (hour>19 and hour<=4)
-    print("Good Night Raju !")
+
+# Adjust the greeting based on the current time
+if 4 < hour <= 12:
+    print("Good Morning Raju!")
+elif 12 < hour <= 15:
+    print("Good Afternoon Raju!")
+elif 15 < hour <= 18:
+    print("Good Evening!")
+elif 19 <= hour or hour <= 4:  # Night condition: either after 7 PM or before 5 AM
+    print("Good Night Raju!")
